2c392b9c1256bec4caec72e7e27329ce21fb9c62
niucan00 <144967966+niucan00@users.noreply.github.com>
2023-09-26 08:34:15 +0800

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,10 @@
+function calculateAverage(number1, number2) {
+    var average = (number1 + number2) / 2;
+    return average;
+}
+
+var num1 = 10;
+var num2 = 20;
+var result = calculateAverage(num1, num2);
+
+console.log("两个数字的平均值是：" + result);
